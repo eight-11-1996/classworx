1c010a164d2da3e3f3d870539f80d1480ceeb3b9
kennCK <kennettecanales@gmail.com>
2018-04-02 08:46:15 +0800

    update

--- api/database/migrations/2018_03_27_064934_create_module_table.php
+++ api/database/migrations/2018_03_27_064934_create_module_table.php
@@ -13,16 +13,16 @@ class CreateModuleTable extends Migration
      */
     public function up()
     {
-        // Schema::create('modules', function (Blueprint $table) {
-        //     $table->increments('id');
-        //     $table->unsignedInteger('parent_id');
-        //     $table->string('description',100);
-        //     $table->string('icon',100);
-        //     $table->string('path',100);
-        //     $table->unsignedInteger('rank');
-        //     $table->timestamps();
-        //     $table->softDeletes();
-        // });
+        Schema::create('modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('parent_id');
+            $table->string('description',100);
+            $table->string('icon',100);
+            $table->string('path',100);
+            $table->unsignedInteger('rank');
+            $table->timestamps();
+            $table->softDeletes();
+        });
         // Artisan::call('db:seed', array('--class' => 'ModulesTableSeeder'));
     }
 
